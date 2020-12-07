0c84d4c40a2d101f3f4d1467406914a8a52ac407
Bram van Rhee <bramvanrhee01@gmail.com>
2020-12-07 21:54:30 +0100

    Opzet voor mvc structuur

--- src/Eindopdracht Java A4/CalculatorController.java
+++ src/Eindopdracht Java A4/CalculatorController.java
@@ -1,4 +1,10 @@
 package Eindopdracht
 
 public class CalculatorController {
+    private CalculatorView View;
+    private CalculatorModel Model;
+
+    public CalculatorController(CalculatorView View, CalculatorModel Model) {
+
+    }
 }
--- src/Eindopdracht Java A4/CalculatorMain.java
+++ src/Eindopdracht Java A4/CalculatorMain.java
@@ -1,4 +1,11 @@
 package Eindopdracht
 
 public class CalculatorMain {
+    public static void main(String[] args) {
+        CalculaterView View = new CalculatorView();
+        CalculatorModel Model = new CalculatorModel();
+        Eindopdracht.CalculatorController Controller = new Eindopdracht.CalculatorController(View, Model);
+
+        View.setVisible(true);
+    }
 }
